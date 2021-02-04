c474524a09628fbd557523e7f4f4e501ed7a5b7e
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2021-02-04 10:55:40 -0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Víctor Manuel Puga Ruiz on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
