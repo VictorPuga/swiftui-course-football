e3e0fab67697f18a0cf611d507891ee05abe5062
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2021-02-04 11:33:28 -0700

    Initial setup

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  // MARK: - Body
+  var body: some View {
+    FooterView()
+      .padding(.horizontal)
+  }
 }
 
+// MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,45 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by Víctor Manuel Puga Ruiz on 04/02/21.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+  // MARK: - Properties
+  
+  
+  
+  // MARK: - Body
+  var body: some View {
+    VStack(alignment: .center, spacing: 10) {
+      Text("We offer the most cutting edge, comfortable, lightweight, and durablr football helments in the market at affordable prices.")
+        .foregroundColor(.gray)
+        .multilineTextAlignment(.center)
+        .layoutPriority(2)
+      
+      Image("logo-lineal")
+        .renderingMode(.template)
+        .foregroundColor(.gray)
+        .layoutPriority(0)
+      
+      Text("Copyright © Victor Puga\nAll rights reserved")
+        .font(.footnote)
+        .foregroundColor(.gray)
+        .multilineTextAlignment(.center)
+        .layoutPriority(1)
+    } // :VStack
+    .padding()
+  }
+}
+
+// MARK: - Preview
+struct FooterView_Previews: PreviewProvider {
+  static var previews: some View {
+    FooterView()
+      .previewLayout(.sizeThatFits)
+      .background(colorBackground)
+  }
+}
